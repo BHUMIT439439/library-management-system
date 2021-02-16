c6f2b6e8e306a6db41bf5fdc6a3bcbb32662a206
bhumit <bnavadiya05@gmail.com>
2021-02-16 18:01:41 +0530

    login module

--- library_management_system/library_management_system/settings.py
+++ library_management_system/library_management_system/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'viewdemoapp',
-    'dbtestapp.apps.DbtestappConfig',
+    'loginmodule.apps.LoginmoduleConfig',
 ]
 
 MIDDLEWARE = [
