ffc938a4874b0cbcaf6d474fba22cd776e70a015
visrut <visrutnavadiya111@gmail.com>
2021-02-20 12:31:49 +0530

    template and url mapping fixed

--- library_management_system/loginmodule/urls.py
+++ library_management_system/loginmodule/urls.py
@@ -3,9 +3,4 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("home",views.home,name="home"),
-    path("login",views.login,name="login"),
-    path("register",views.register,name="register"),
-    # path("/welcome",views.welcome,name="welcome"),
-    
 ] 
