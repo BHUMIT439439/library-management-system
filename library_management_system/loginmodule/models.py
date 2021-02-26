23a19e35760960bd6d7cb702d57ebd9f49559230
bhumit <bnavadiya05@gmail.com>
2021-02-26 19:23:01 +0530

    update database table in loginmodule

--- library_management_system/loginmodule/models.py
+++ library_management_system/loginmodule/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+class Reader(models.Model):
+    username = models.CharField(primary_key=True,max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20)
 
-# Create your models here.
+
