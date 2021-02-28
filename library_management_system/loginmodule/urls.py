bd11006fda232009ba8afbd391e7fd8f7d96ca21
visrut <visrutnavadiya@gmail.com>
2021-02-28 14:49:40 +0530

    urls.py clean up

--- library_management_system/loginmodule/urls.py
+++ library_management_system/loginmodule/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 app_name = 'loginmodule'
 urlpatterns = [
-	path('',include(loginmodule.views)),
+	path('',views.home,name='home'),
+	path('home/',views.home,name='home'),
+	path('register/',views.register,name='register'),
+	path('login/',views.login,name='login'),
+	path('welcome/',views.welcome,name='welcome'),
+	path('failure/',views.failure,name='failure'),
+	path('welcome/home.html',views.home,name='home'),
 ] 
