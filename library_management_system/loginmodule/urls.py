63007c838e920c1405fc5aab96b8e19ed0979317
visrut <visrutnavadiya@gmail.com>
2021-02-28 14:29:26 +0530

    urls.py changed

--- library_management_system/loginmodule/urls.py
+++ library_management_system/loginmodule/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from . import views
 
+app_name = 'loginmodule'
 urlpatterns = [
+	path('',include(loginmodule.views)),
 ] 
