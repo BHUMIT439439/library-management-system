f6f2a18f742ef4e2cbe25b1bd59ee7a4b083e717
bhumit <bnavadiya05@gmail.com>
2021-03-02 15:51:46 +0530

    book module started

--- library_management_system/loginmodule/models.py
+++ library_management_system/loginmodule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Reader(models.Model):
     username = models.CharField(primary_key=True,max_length=20)
--- library_management_system/loginmodule/urls.py
+++ library_management_system/loginmodule/urls.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+   path('',views.home,name='home'),
+   path('register/',views.register,name='register'),
+   path('login/',views.login,name='login'),
+   path('welcome/',views.welcome,name='welcome'),
+   path('failure/',views.failure,name='failure'),
 ] 
