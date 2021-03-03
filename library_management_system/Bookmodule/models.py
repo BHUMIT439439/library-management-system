bc198c1bc1e3ef9f60aae5e92825d693854f7488
visrut <visrutnavadiya@gmail.com>
2021-03-03 13:26:41 +0530

    removeBook added

--- library_management_system/Bookmodule/models.py
+++ library_management_system/Bookmodule/models.py
@@ -5,9 +5,9 @@ class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
     book_name = models.CharField(max_length=100)
     auther_name = models.CharField(max_length=100)
-    publish_year = models.IntegerField(max_length=4)
+    publish_year = models.IntegerField()
 
 class issueBook(models.Model):
-    issue_book_id = models.IntegerField()
+    # issue_book_id = models.IntegerField()
     issue_book_name = models.CharField(max_length=100)
     reader_name= models.ForeignKey(Reader, on_delete=models.CASCADE)
--- library_management_system/Bookmodule/views.py
+++ library_management_system/Bookmodule/views.py
@@ -8,14 +8,20 @@ def addBook(request):
         book_name = request.POST['bname']
         auther_name = request.POST['aname']
         publish_year = request.POST['pyear']
+        super_pwd = request.POST['superpassword']
         if Book.objects.filter(book_name = book_name).exists() and Book.objects.filter(auther_name = auther_name).exists():
             messages.info(request,'book already exist')
             return render(request,'addBook.html')
         else:
-            b = Book(book_name = book_name , auther_name = auther_name , publish_year = publish_year)
-            b.save()
-            messages.info(request,'successfully book added')
-            return render(request,'addBook.html')
+            if super_pwd == 'xyz':
+                b = Book(book_name = book_name , auther_name = auther_name , publish_year = publish_year)
+                b.save()
+                book_id = Book.objects.filter(book_name=book_name).first().book_id
+                messages.info(request,f'successfully book added and id is {book_id}')
+                return render(request,'addBook.html')
+            else:
+                messages.info(request,'superuser Password is invalid')
+                return render(request,'addBook.html')
     else:
          return render(request,'addBook.html')
                 
@@ -35,4 +41,21 @@ def issueBook(request):
 def returnBook(request):
     return render(request,'returnBook.html')
 
-
+def removeBook(request):
+    if request.method == "POST":
+        book_id = request.POST['book_id']
+        super_pwd = request.POST['superpassword']
+        if Book.objects.filter(book_id = book_id).exists():
+            if super_pwd == 'xyz':
+                messages.info(request,'book is removed')
+                b = Book.objects.filter(book_id=book_id).first()
+                b.delete()
+                return render(request,'removeBook.html')
+            else:
+                messages.info(request,'invalid password')
+                return render(request,'removeBook.html')
+        else:
+            messages.info(request,'book not exists')
+            return render(request,'removeBook.html')
+    else:
+        return render(request,'removeBook.html')
