9f2c8027b600598cef84768b1dc2ba325c0383a9
visrut <visrutnavadiya@gmail.com>
2021-03-07 15:28:19 +0530

    issue book functionality improved

--- library_management_system/Bookmodule/models.py
+++ library_management_system/Bookmodule/models.py
@@ -12,3 +12,6 @@ class IssueBook(models.Model):
     issue_id = models.ForeignKey(Book, on_delete=models.CASCADE,null=True)
     reader_name= models.ForeignKey(Reader, on_delete=models.CASCADE,null=True)
     date = models.DateField(("Date"), default=datetime.date.today)
+
+    def __str__(self):
+    	return f"[{self.reader_name} => {self.issue_id.book_name}]"
